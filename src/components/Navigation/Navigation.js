83188a29bf061ff3c5c6e8ff6f79c01727a60fa8
James Thorderson <jamesdthor@gmail.com>
2018-11-20 23:46:02 -0600

    initializing facerec front end

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+		if (isSignedIn) {
+			return(
+				<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+					<p onClick={() => onRouteChange('signin')} 
+						className='f4 link dim black pa3 pointer'>SIGN OUT</p>
+				</nav>
+				);
+		} else {
+			return(
+				<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+					<p onClick={() => onRouteChange('signin')} 
+						className='f4 link dim black pa3 pointer'>SIGN IN</p>
+					<p onClick={() => onRouteChange('register')} 
+						className='f4 link dim black pa3 pointer'>REGISTER</p>
+				</nav>
+			);
+		}	
+}
+
+export default Navigation;
